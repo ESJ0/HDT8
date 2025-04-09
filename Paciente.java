cc9b29e30f5e143dda807d57f899e5652e960a95
ESJ0 <usuarioj0s3p1@gmail.com>
2025-04-09 12:40:42 -0600

    Se creo la clase "Paciente" con sus respectivos atrbutos y metodos.

---
+++ Paciente.java
@@ -0,0 +1,28 @@
+public class Paciente implements Comparable<Paciente> {
+    private String nombre;
+    private String sintoma;
+    private char codigo;
+
+    public Paciente(String nombre, String sintoma, char codigo) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigo = codigo;
+    }
+
+    public String getNombre(){ 
+        return nombre;}
+
+    public String getSintoma(){ 
+        return sintoma;}
+
+    public char getCodigo(){ 
+        return codigo;}
+
+    public int compareTo(Paciente otro) {
+        return Character.compare(this.codigo, otro.codigo);
+    }
+
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + codigo;
+    }
+}
